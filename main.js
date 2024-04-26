bb681c0a96dec0f2857cfa0de7f31342c8cf4def
Muhammad Ibrahim <160318731+Muhammad-Ibrahim99@users.noreply.github.com>
2024-04-26 17:23:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("guess a number between 1 to 10");
+//creating variables:
+let a = Math.floor(Math.random() * 10);
+import inquirer from "inquirer";
+while (true) {
+    let input = await inquirer.prompt({ name: "guess", type: "number", message: "enter your guess number:" });
+    //code for given number:
+    let ans = input.guess;
+    if (ans == a) {
+        console.log("Congratulations your guess number is correct");
+        break;
+    }
+    else {
+        console.log("Sorry try again");
+    }
+    ;
+    //adding hint:
+    if (a > ans) {
+        console.log("hint: your number is lower");
+    }
+    else {
+        console.log("hint: your number is higher");
+    }
+    console.log("GAME OVER");
+}
+;
